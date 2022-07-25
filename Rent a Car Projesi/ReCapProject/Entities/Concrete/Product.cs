d4ed0a74736b6b258a1114ec14d6875bbb9160dc
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2022-07-26 00:34:53 +0300

    ReCapProject oluşturuldu.

---
+++ Rent a Car Projesi/ReCapProject/Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity
+    {
+        public int  ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int BrandID { get; set; }
+        public int ColorId { get; set; }
+        public int ModelYear { get; set; }
+        public int DailyPrice { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,29 @@
+﻿--select komutu bir veriyi çekmek için kullanılır
+Select ContactName,CompanyName,City from Customers
+
+-- kullanıcıları seç,
+Select*from Customers where City='Berlin'
+
+Select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products where CategoryID=1 or CategoryID=3
+
+-- order by sırala demek.
+-- kategori ıd ve ürün ismine göre sırala
+select * from Products order by  CategoryID,ProductName
+
+-- fiyata göre sırala
+
+select * from Products order by UnitPrice asc -- ascending (artan) sırala
+
+select * from Products order by UnitPrice desc -- descending (azalan) sırala
+
+select count(*) Adet  from Products where CategoryID=1
+
+
+-- hangi kategoride kaç farklı ürün var
+-- group by kullanılırken kategori ettiğiniz kısım yazılır
+-- her bir eleman için adet bilgisi alınır. count adet bilgisini verir.
+-- group by da koşuş having ile yazılır
+select CategoryId, count(*) Adet from  Products group by CategoryID having count(*)<10
+
